89a978aa2bbe71ebb7df67362f07df123a7ae8c8
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-04-17 15:14:00 +0700

    set up font DM Sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import './globals.css';
 import { PrismicPreview } from '@prismicio/next';
 import { repositoryName } from '@/prismicio';
+import { DM_Sans } from 'next/font/google'
+ 
+const dmSans = DM_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-dm-sans',
+})
 
 export default function RootLayout({
   children,
@@ -8,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={dmSans.variable}>
       <body>{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/slices/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/slices/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-dm-sans)"],
+      },
+    },
   },
   plugins: [],
 };
