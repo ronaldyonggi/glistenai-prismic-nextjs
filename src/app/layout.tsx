389c77be692dedd28535819e7a17f3c079fa075a
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-04-17 14:41:25 +0700

    set up tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import { PrismicPreview } from "@prismicio/next";
-import { repositoryName } from "@/prismicio";
+import './globals.css';
+import { PrismicPreview } from '@prismicio/next';
+import { repositoryName } from '@/prismicio';
 
 export default function RootLayout({
   children,
